aa4f64fbc91b784064a7b5f1f03d326faf4878e3
ghyoon93 <gh.yoon93@gmail.com>
2024-09-23 21:31:44 +0900

    [sso][config] : 주석 코드 제거

--- src/main/java/com/example/config/UiSecurityConfig.java
+++ src/main/java/com/example/config/UiSecurityConfig.java
@@ -1,12 +1,9 @@
 package com.example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * @주시스템 :   client-app
@@ -21,10 +18,6 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 @EnableOAuth2Sso // OAuth 2.0을 사용한 Single Sign-On(SSO)을 활성화합니다.
 public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private OAuth2ClientContext oauth2ClientContext;
-
-	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
 		http.antMatcher( "/**" ) // 모든 요청에 이 구성 적용
@@ -41,27 +34,6 @@ public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
 						response.sendRedirect("http://sso.abc.com:8081/auth/logout" ) )
 				.invalidateHttpSession(true)
 				.permitAll();
-
-//		http.antMatcher( "/**" ) // 모든 요청에 이 구성 적용
-//				.authorizeRequests() // 요청에 대한 인증 규칙 정의
-//				.antMatchers( "/", "/login**" )
-//				.permitAll() // 루트와 로그인 관련 경로는 인증 규칙에서 제외
-//				.anyRequest()
-//				.authenticated()  // 그외 모든 요청에 대해서 인증이 필요
-//				.and()
-//				.logout()
-//					.logoutSuccessHandler((request, response, authentication) -> {
-//						OAuth2AccessToken accessToken = oauth2ClientContext.getAccessToken();
-//						if (accessToken != null) {
-//							String logoutUrl = "http://sso.abc.com:8081/logout?token=" + accessToken.getValue();
-//							response.sendRedirect(logoutUrl);
-//						}
-//					})
-//				.invalidateHttpSession(true)
-//				.clearAuthentication(true)
-//				.deleteCookies("JSESSIONID")
-//				.and()
-//				.csrf().disable();
 	}
 
 }
